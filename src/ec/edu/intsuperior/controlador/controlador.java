0d46ea8040d760d32c8e3ef8439e90197f584bba
Anthony Sanchez <Anthony Sanchez@DESKTOP-191A81L.mshome.net>
2023-08-08 18:20:15 -0500

    proyecto final

---
+++ src/ec/edu/intsuperior/controlador/controlador.java
@@ -0,0 +1,30 @@
+package ec.edu.intsuperior.controlador;
+
+import ec.edu.intsuperior.vista.FmrAdministrador;
+import ec.edu.intsuperior.vista.FmrLogin;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class controlador {
+    private FmrLogin Fmrlogin = new FmrLogin(this);
+    private FmrAdministrador FmrAdministrador = new FmrAdministrador(this);
+    public void mostrarAdministrador(){
+        FmrAdministrador.setVisible(true);
+    }
+    public void ocultarAdministrador(){
+        FmrAdministrador.setVisible(false);
+    }
+    
+    public void Mostrarlogin(){
+        Fmrlogin.setVisible(true);
+    }
+    public void ocultarlogin(){
+        Fmrlogin.setVisible(false);
+    }
+    }
+
+    
+            
+   
---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.controlador.controlador;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+       controlador c= new controlador();
+       c.Mostrarlogin();
+       
+    }
+}
